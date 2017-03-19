137dc6650fb669b6749c2d58e552fd8518c7c41a
AdamNotebook <AdamNotebook@DESKTOP-J4E36MQ>
2017-03-19 18:11:59 +0900

    세팅완료

---
+++ camsns/src/main/java/com/spring/camsns/HomeDTO.java
@@ -0,0 +1,14 @@
+package com.spring.camsns;
+
+public class HomeDTO {
+	private String userEmailId;
+
+	public String getUserEmailId() {
+		return userEmailId;
+	}
+
+	public void setUserEmailId(String userEmailId) {
+		this.userEmailId = userEmailId;
+	}
+	
+}
